b8270a99b8e08f914001f72f605f40fa4758d88a
letungduong24 <2251172311@e.tlu.edu.vn>
2025-11-24 17:20:30 +0700

    add user types

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,2 +1,5 @@
 export * from "./products/dtos/create-product.request";
 export * from "./products/interfaces/product.interface";
+export * from "./user/dtos/signin.request";
+export * from "./user/dtos/signup.request";
+export * from "./user/interfaces/user.interface";
---
+++ packages/types/src/user/dtos/signup.request.ts
@@ -0,0 +1,7 @@
+export class SignUpRequest {
+  email!: string;
+  password!: string;
+  name?: string;
+}
+
+
